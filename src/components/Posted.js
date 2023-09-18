b8f78c9921b4a536471d54a193ec85dd39098cf2
SiriChandhana06 <sirisiri3006@gmail.com>
2023-09-18 20:35:37 +0530

    First_commit

---
+++ src/components/Posted.js
@@ -0,0 +1,9 @@
+import React from "react";
+function  Posted(props) {
+    <div>
+        <div>{props._name}</div>
+        <div>{props._desc}</div>
+        <div>{props._price}</div>
+    </div>
+
+}
